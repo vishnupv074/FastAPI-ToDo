5c8462d3f56cf97dd197c9db1017e73e7108aec3
vishnupv074 <vishnupv074@gmail.com>
2025-01-01 23:05:50 +0530

    Dependancy Injection and databse read - How the depandancy injection work - How to connect  the data base session - how to get the data

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
-from app import models
-from .database import engine
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
+from .models import Base, Todos
+from .database import engine, SessionLocal
 
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependancy = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/")
+async def read_all(db: db_dependancy):
+    return db.query(Todos).all()
