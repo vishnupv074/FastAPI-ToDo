6e24c97f507bb7b39a4b40c80e748dd82d890fbe
vishnupv074 <vishnupv074@gmail.com>
2025-01-11 01:31:50 +0530

    User authentication - 125

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -6,6 +6,7 @@ from passlib.context import CryptContext
 from app.database import SessionLocal
 from sqlalchemy.orm.session import Session
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 router = APIRouter()
@@ -25,6 +26,15 @@ def get_db():
 db_dependancy = Annotated[Session, Depends(get_db)]
 
 
+def authenticate_user(username, password, db) -> bool:
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return None
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+
 class CreateUserRequest(BaseModel):
     username: str
     email: str
@@ -41,15 +51,16 @@ class CreateUserRequest(BaseModel):
                 "first_name": "Vishnu",
                 "last_name": "P V",
                 "password": "pass123",
-                "role": "admin"
+                "role": "admin",
             }
         }
     }
 
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
-async def create_user(db: db_dependancy,
-                      create_user_request: CreateUserRequest):
+async def create_user(
+    db: db_dependancy, create_user_request: CreateUserRequest
+):
     # create_user_model = Users(**create_user_request.model_dump())
     create_user_model = Users(
         email=create_user_request.email,
@@ -63,3 +74,14 @@ async def create_user(db: db_dependancy,
 
     db.add(create_user_model)
     db.commit()
+
+
+@router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: db_dependancy,
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "User not authenticated!"
+    return "Successful Authentication"
