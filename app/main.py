5fee42667b6372f9e92c173ed8d6766574b90cd9
vishnupv074 <vishnupv074@gmail.com>
2025-01-26 15:29:08 +0530

    Users app included and get user and password change

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .models import Base
 from .database import engine
-from .routers import auth, todos, admin
+from .routers import auth, todos, admin, users
 
 
 app = FastAPI()
@@ -11,3 +11,4 @@ Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
+app.include_router(users.router)
---
+++ app/routers/users.py
@@ -0,0 +1,136 @@
+from typing import Annotated
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException, Path, status
+from app.models import Todos, Users
+from app.database import SessionLocal
+from .auth import get_current_user, bcrypt_context
+
+
+router = APIRouter(prefix="/user", tags=["users"])
+"""
+Initializes an APIRouter instance for admin-related operations.
+
+- `prefix`: Specifies a URL prefix for all endpoints under this router.
+  Example: "/admin".
+- `tags`: Adds tags to categorize endpoints, here it uses "admin".
+
+Returns:
+    APIRouter: An instance of `APIRouter` with the specified prefix
+    and tags.
+"""
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Dependancy injection
+db_dependancy = Annotated[Session, Depends(get_db)]
+user_dependancy = Annotated[dict, Depends(get_current_user)]
+
+
+class UserVerification(BaseModel):
+    password: str
+    new_password: str = Field(min_length=6)
+
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_user(
+    user: Annotated[dict, Depends(get_current_user)], db: db_dependancy
+):
+    """
+    Fetches the user details from the database.
+
+    Args:
+        user: Authenticated user dependency.
+        db: Database session dependency.
+
+    Raises:
+        HTTPException: If user authentication fails.
+
+    Returns:
+        User: The authenticated user details from the database.
+    """
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Authentication failed!",
+        )
+
+    username = user.get("username")
+    user_id = user.get("id")
+
+    if not username or not user_id:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid user data!",
+        )
+
+    user_record = (
+        db.query(Users)
+        .filter(Users.username == username, Users.id == user_id)
+        .first()
+    )
+
+    if user_record is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found!"
+        )
+
+    return user_record
+
+
+@router.put("/password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(
+    user: user_dependancy,
+    db: db_dependancy,
+    user_verification: UserVerification,
+):
+    """
+    Changes the password for the authenticated user.
+
+    Args:
+        user: Authenticated user dependency.
+        db: Database session dependency.
+        user_verification: UserVerification containing the old and new
+                           passwords.
+
+    Raises:
+        HTTPException: If authentication or password verification fails.
+    """
+    # Check if the user is authenticated
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Authentication failed!",
+        )
+
+    # Fetch the authenticated user's record from the database
+    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
+    if user_model is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Authentication failed!",
+        )
+
+    # Verify the current password provided by the user
+    if not bcrypt_context.verify(
+        user_verification.password, user_model.hashed_password
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Error in password change!",
+        )
+
+    # Hash the new password and update the user's record with it
+    user_model.hashed_password = bcrypt_context.hash(
+        user_verification.new_password
+    )
+
+    # Commit the changes to the database
+    db.commit()
